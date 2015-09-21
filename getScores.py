22990cab23e690fddcff323a9905d40522e3cf12
Daniel Young <dmyoung1994@gmail.com>
2015-09-21 10:44:25 -0400

    edit pyhton server script

--- getScores.py
+++ getScores.py
@@ -8,30 +8,9 @@ from flask import Response
 from flask import send_from_directory
 
 app = Flask(__name__)
-
-def root_dir():  # pragma: no cover
-    return os.path.abspath(os.path.dirname(__file__))
-
-def get_file(filename):  # pragma: no cover
-    try:
-        src = os.path.join(root_dir(), filename)
-        # Figure out how flask returns static files
-        # Tried:
-        # - render_template
-        # - send_file
-        # This should not be so non-obvious
-        return open(src).read()
-    except IOError as exc:
-        return str(exc)
-
 @app.route("/public/<path:path>")
 def sendpublic(path):
 	return send_from_directory('public', path)
-
-@app.route("/")
-def fun():
-    content = get_file('index.html')
-    return Response(content, mimetype="text/html")
 
 @app.route("/users")
 def get():
@@ -66,7 +45,7 @@ def put():
 		query = "update users set score=" + score + "  where id=" + id + ";"
 
 	print(query)
-		
+
 	cursor.execute(query)
 	cnxn.commit()
 	cnxn.close()
