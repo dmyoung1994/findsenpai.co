d87ba7214d54a9566e7c1c13243ed3a46eb48371
Daniel Young <dmyoung1994@gmail.com>
2015-09-21 15:16:21 -0400

    adding stuff to server

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ function buildUsers(rows) {
   return users;
 }
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
 app.get('/users', function(req, res) {
   conn.connect();
   var users = [];
